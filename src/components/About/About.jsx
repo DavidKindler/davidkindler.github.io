6afc897fe4b794c4128ffaeca9847c651a674293
David Kindler <david.kindler@nxp.com>
2019-12-30 14:23:49 -0600

    initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './About.css'
+
+class About extends Component {
+  render () {
+    return (
+      <div className='about'>
+        <h1>This is about me.</h1>
+      </div>
+    )
+  }
+}
+
+export default About
