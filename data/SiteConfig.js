fd4bd4d09f406d278f0880ececc13acc63b02bfa
David Kindler <david.kindler@nxp.com>
2019-12-31 13:28:22 -0600

    update disqus comments component

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,7 +3,7 @@ const config = {
   siteTitleShort: "DK Blog", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "Blog site for David Kindler", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://davidkindler.com", // Domain of your website without pathPrefix.
+  siteUrl: "https://davidkindler.github.io", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "A blog site with advanced design in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
--- src/components/Disqus/Disqus.jsx
+++ src/components/Disqus/Disqus.jsx
@@ -1,51 +1,106 @@
-import React, { Component } from "react";
-import ReactDisqusComments from "react-disqus-comments";
-import urljoin from "url-join";
-import config from "../../../data/SiteConfig";
+/* eslint-disable react/jsx-fragments */
+import React, { Component, Fragment } from 'react'
+import ReactDisqusComments from 'disqus-react'
+import urljoin from 'url-join'
+import config from '../../../data/SiteConfig'
 
 class Disqus extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      toasts: []
-    };
-    this.notifyAboutComment = this.notifyAboutComment.bind(this);
-    this.onSnackbarDismiss = this.onSnackbarDismiss.bind(this);
-  }
-
-  onSnackbarDismiss() {
-    const [, ...toasts] = this.state.toasts;
-    this.setState({ toasts });
-  }
-
-  notifyAboutComment() {
-    const toasts = this.state.toasts.slice();
-    toasts.push({ text: "New comment available!" });
-    this.setState({ toasts });
-  }
-
-  render() {
-    const { postNode } = this.props;
+  render () {
     if (!config.disqusShortname) {
-      return null;
+      return null
     }
-    const post = postNode.frontmatter;
-    const url = urljoin(
-      config.siteUrl,
-      config.pathPrefix,
-      postNode.fields.slug
-    );
+    const { postNode } = this.props
+    const post = postNode.frontmatter
+    const url = urljoin(config.siteUrl, config.pathPrefix, postNode.fields.slug)
+
+    const { disqusShortname } = config
+
+    const disqusConfig = {
+      url,
+      identifier: post.title,
+      title: post.title
+    }
+
+    // const { postNode } = this.props
+    //     if (!config.disqusShortname) {
+    //       return null;
+    //     }
+    //     const post = postNode.frontmatter;
+    //     const url = urljoin(
+    //       config.siteUrl,
+    //       config.pathPrefix,
+    //       postNode.fields.slug
+    //     );
+    //     ret
+
     return (
-      <ReactDisqusComments
-        shortname={config.disqusShortname}
-        identifier={post.title}
-        title={post.title}
-        url={url}
-        category_id={post.category_id}
-        onNewComment={this.notifyAboutComment}
-      />
-    );
+      <Fragment>
+        {/* // <div className="article">
+      //     <h1>{this.props.article.title}</h1>
+      //     <Disqus.CommentCount shortname={disqusShortname} config={disqusConfig}>
+      //         Comments
+      //     </Disqus.CommentCount>
+
+      //     <p>{this.props.article.body}</p> */}
+
+        {/* <ReactDisqusComments.CommentEmbed
+          // commentId={this.props.article.featuredComment}
+          showMedia
+          height={160}
+        /> */}
+        {/* {JSON.stringify(disqusConfig, null, 2)} */}
+        <ReactDisqusComments.DiscussionEmbed
+          shortname={disqusShortname}
+          config={disqusConfig}
+        />
+        {/* // </div> */}
+      </Fragment>
+    )
   }
 }
+// class Disqus extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       toasts: []
+//     };
+//     this.notifyAboutComment = this.notifyAboutComment.bind(this);
+//     this.onSnackbarDismiss = this.onSnackbarDismiss.bind(this);
+//   }
 
-export default Disqus;
+//   onSnackbarDismiss() {
+//     const [, ...toasts] = this.state.toasts;
+//     this.setState({ toasts });
+//   }
+
+//   notifyAboutComment() {
+//     const toasts = this.state.toasts.slice();
+//     toasts.push({ text: "New comment available!" });
+//     this.setState({ toasts });
+//   }
+
+//   render() {
+//     const { postNode } = this.props;
+//     if (!config.disqusShortname) {
+//       return null;
+//     }
+//     const post = postNode.frontmatter;
+//     const url = urljoin(
+//       config.siteUrl,
+//       config.pathPrefix,
+//       postNode.fields.slug
+//     );
+//     return (
+//       <ReactDisqusComments
+//         shortname={config.disqusShortname}
+//         identifier={post.title}
+//         title={post.title}
+//         url={url}
+//         category_id={post.category_id}
+//         onNewComment={this.notifyAboutComment}
+//       />
+//     );
+//   }
+// }
+
+export default Disqus
